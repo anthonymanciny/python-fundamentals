e4f92e95883b08dac9a109d43763e13eb85fd402
anthonymanciny <141266961+anthonymanciny@users.noreply.github.com>
2024-07-17 15:21:53 +0000

    QWERTY

--- 03_funcao.py
+++ 03_funcao.py
@@ -1,2 +1,17 @@
-def teste_supremo():
-    print("asd")
+def verificador_ano_bissexto():
+    ano = int(input())
+
+# TODO: Verifique se o ano é bissexto utilizando uma estrutura de controle condicional, como if/else:
+
+
+verificador_ano_bissexto()
+if  ano % 4 == 0: 
+      if ano % 100 == 0:
+        if ano % 400 == 0:
+          print ("SIM")
+        else:
+          print ("NÃO")
+      else:
+        print ("SIM")
+else:
+      print( "NÃO")
