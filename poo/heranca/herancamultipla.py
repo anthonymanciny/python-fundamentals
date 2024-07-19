fa6d1aae7fe39c7d567cb0ed00617288a647f266
anthonymanciny <141266961+anthonymanciny@users.noreply.github.com>
2024-07-19 14:54:53 +0000

    qwerety

--- poo/heranca/herancamultipla.py
+++ poo/heranca/herancamultipla.py
@@ -3,22 +3,37 @@ class Animal:
     def __init__(self, nro_patas) :
         self.nro_patas = nro_patas
 
+    def __str__(self):
+        return f"{self.__class__.__name__}: {','.join([f'{chave} = {valor}' for chave, valor in self.__dict__.items()])}"
+
+# =====================================================================================
+
 class Mamifero(Animal):
-     def __init__(self, nro_patas) :
-        self.nro_patas = nro_patas
-        super().__init__(nro_patas)
-
-
+     def __init__(self, cor_pelo, **kw) :
+       self.cor_pelo = cor_pelo
+       super().__init__(**kw) 
+       
 class Ave(Animal):
-     def __init__(self, nro_patas) :
-        self.nro_patas = nro_patas
-        super().__init__(nro_patas)
+     def __init__(self, cor_bico,**kw) :
+        self.cor_bico = cor_bico
+        super().__init__(**kw)
 
-class Cachorro(Mamifero):
+# ========================================================================================== 
+
+class Gato(Mamifero):
     pass
 
 class Ornitorrinco(Mamifero, Ave):
     pass
         
-cachorro = Cachorro(4)
-print(cachorro)
+# =============================================================================================
+
+gato = Gato( 
+                nro_patas= 4, cor_pelo ="Preto")
+print(gato)
+
+ornitorrinco = Ornitorrinco(
+                            nro_patas =2, 
+                            cor_bico= "Azul",
+                            cor_pelo="Vermelho")
+print(ornitorrinco)
